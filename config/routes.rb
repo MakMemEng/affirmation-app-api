56d12de05d9d226258bc2ac37d33db6bf17083ff
makmemeng <yatani.mems4568@gmail.com>
2021-12-01 12:41:47 +0900

    Edit post controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :posts
+      resources :posts, only:[:index]
 
       # auth_token
       resources :auth_token, only:[:create] do
