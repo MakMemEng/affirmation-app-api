739cef6ef42bf497cc2289609fcba70887c979e5
makmemeng <yatani.mems4568@gmail.com>
2021-12-08 13:17:58 +0900

    Fix PostAPI controller

--- app/controllers/api/v1/post_controller.rb
+++ app/controllers/api/v1/post_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::PostController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  # before_action :authenticate_user!, except: [:index]
 
-  # # N+1問題の対応
+  # N+1問題の対応
   def index
-    @posts = Post.preload(:users)
+    @posts = Post.all.includes(:user)
+    # @posts = Post.includes(:user).order(id: "DESC").page(params[:page]).per(25)
     render json: @posts, each_serializer: PostsSerializer
   end
 
@@ -13,7 +14,7 @@ class Api::V1::PostController < ApplicationController
       render json: @post, each_serializer: PostsSerializer
     else
       render json: @post.errors, status: 500
-  　end
+    end
   end
 
   private
