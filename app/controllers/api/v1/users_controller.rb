48f67e0750fb14db24e68fdbab7cee0baf1993ca
makmemeng <yatani.mems4568@gmail.com>
2021-12-11 15:16:04 +0900

    Edit users controller for signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authenticate_user
+  # before_action :authenticate_user
   # before_action :ensure_normal_user, only: %i[update destroy]
 
-  def index
-    # @users = User.all
-    render json: current_user.as_json(only: [:id, :name, :email, :created_at])
-  end
-
   # def index
-  #   @users = User.all
-  #   render json: @users, each_serializer: UsersSerializer
+  #   # @users = User.all
+  #   render json: current_user.as_json(only: [:id, :name, :email, :created_at])
   # end
+
+  def index
+    @users = User.all
+    render json: @users, each_serializer: UsersSerializer
+  end
 
   def show
     @user = User.find(params[:id])
@@ -29,10 +29,19 @@ class Api::V1::UsersController < ApplicationController
     render :edit
   end
 
+  def create
+    @users = User.create(user_params)
+    return render_400 if status == 400
+    @users.save
+    render json: {status: 200, message: 'ユーザー登録に成功しました。'}, status: 200
+  rescue StandardError => e
+    Rails.logger.error(e.message)
+    render json: Init.message(500, e.message), status: 500
+  end
+
   def destroy
     @user = User.find(params[:id]).destroy
     render json: { status: 'SUCCESS', message: 'ユーザー情報を削除しました。', data: @user }
-    # flash[:success] = "ユーザー情報を削除しました。"
     redirect_to users_path
   end
 
@@ -44,6 +53,7 @@ class Api::V1::UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :profile, :profile_image)
+      params.require(:user).permit(:name, :email, :password)
+      # params.require(:user).permit(:name, :email, :profile, :profile_image)
     end
 end
