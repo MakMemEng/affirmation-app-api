f33ad6982545ffd8ce8661f11176dae3241ad4be
makmemeng <yatani.mems4568@gmail.com>
2022-02-03 13:31:52 +0900

    dev env

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
+# Doc: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#config
+
 workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
 
 max_threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS") { 5 })
@@ -7,26 +9,15 @@ threads min_threads_count, max_threads_count
 preload_app!
 
 rackup      DefaultRackup
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
-# Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RACK_ENV") { "development" }
 
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+port        ENV.fetch("PORT") { 3000 }
+
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection
 end
-
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
-
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
-
-app_root = File.expand_path('..', __dir__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
